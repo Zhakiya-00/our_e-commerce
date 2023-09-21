1df2a9463bad7a63824194b7186014b342e6ceb6
Makinde Oluwasegun <bryanconquer@gmail.com>
2023-09-21 11:13:58 +0100

    Readme

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,7 +1,7 @@
-var updateBtns = document.getElementsByClassName('update-cart')
+const updateBtns = document.getElementsByClassName('update-cart')
 
 for (i = 0; i < updateBtns.length; i++) {
-	updateBtns[i].addEventListener('click', function(){
+	updateBtns[i].addEventListener('click', () => {
 		var productId = this.dataset.product
 		var action = this.dataset.action
 		console.log('productId:', productId, 'Action:', action)
